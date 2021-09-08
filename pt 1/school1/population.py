0e36404f1bb0b342bb94c6366fe1ed23bd96269d
Laavanya-Agarwal <71713140+Laavanya-Agarwal@users.noreply.github.com>
2021-09-08 17:54:28 +0530

    Add files via upload

---
+++ pt 1/school1/population.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import csv
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import random
+import statistics
+
+df = pd.read_csv('School1.csv')
+data = df['Math_score'].tolist()
+
+fig = ff.create_distplot([data], ['Math Score'], show_hist = False)
+fig.show()
+
+mean = statistics.mean(data)
+print('Mean: ' + mean)
+sd = statistics.stdev(data)
+print('Mean: ' + sd)
---
+++ pt 1/school1/sample.py
@@ -0,0 +1,38 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("School1.csv")
+data = df["Math_score"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    
+    mean = statistics.mean(dataset)
+    return mean
+
+def mean():
+    mean_list = []
+    for i in range(0,100):
+        set_of_means = random_set_of_mean(30)
+        mean_list.append(set_of_means)
+    
+    df = mean_list
+    mean = statistics.mode(df)
+    fig = ff.create_distplot([df], ["Math Score"], show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.15], mode="lines", name="Mean"))
+    fig.show()
+
+    population_mean = statistics.mean(data)
+    print("Sampling mean: ", population_mean)
+    
+    population_sd = statistics.stdev(data)
+    print("Sampling standard deviation: ", population_sd)
+mean()
